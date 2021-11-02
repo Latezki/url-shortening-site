4d59c3db54c665e48ec5c5d6986563a5d491b0b3
Latezki <kovanen.lauri@gmail.com>
2021-11-02 20:13:45 +0200

    Added the short link copy functionality to copy button

--- index.js
+++ index.js
@@ -4,18 +4,11 @@ const urlInput = document.getElementById("inputText");
 const formWarning = document.getElementById("formWarning");
 const buttonS = document.getElementsByClassName("copy-button");
 
-var linkArray =[];
-var linkItems = JSON.parse(localStorage.getItem("links"));
-
 window.addEventListener("load", function() {
-    for (var i = 0; i < linkItems.length; i++) {
-        const li = document.createElement("li");
-        li.innerHTML = linkItems[i];
-        linkList.appendChild(li);
-        linkArray.push(li.innerHTML);
+    const storageLinkList = localStorage.getItem("links");
+    if (!linkList.innerHTML) {
+        linkList.innerHTML = storageLinkList;
     }
-
-    localStorage.setItem("links", JSON.stringify(linkArray));
 });
 
 for (var i = 0 ; i < linkList.length; i++) {
@@ -42,34 +35,23 @@ linkForm.addEventListener("submit", function(e) {
     }
 });
 
-async function newLink(urlName) {
-    const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${urlName}`);
-    response.json().then(data => {
-        const li = document.createElement("li");
-        const url = document.createElement("p");
-        const shortLink = document.createElement("p");
-        // const button = document.createElement("button");
-        li.className = "link-li"
-        url.className = "website-link"
-        url.innerHTML = urlName
-        shortLink.className = "shortened-link";
-        shortLink.innerHTML = data.result.full_short_link;
-        // button.className = "copy-button";
-        // button.innerHTML = "Copy";
-        li.appendChild(url);
-        li.appendChild(shortLink);
-        // li.appendChild(button);
-        linkList.appendChild(li);
-
-        linkArray.push(li.innerHTML);
-        localStorage.setItem("links", JSON.stringify(linkArray));
-
-        /*
-        button.addEventListener("click", function(e) {
-            navigator.clipboard.writeText(shortLink.innerHTML);
-        });
-        */
+function newLink(urlName) {
+    fetch(`https://api.shrtco.de/v2/shorten?url=${urlName}`)
+    .then(res => res.json())
+    .then(data => {
+        linkListLi = document.createElement("li");
+            linkListLi.className = "link-li";
+            linkListLi.innerHTML = `<p class="website-link">${data.result.original_link}</p>
+            <p class="shortened-link">${data.result.full_short_link}</p>
+            <button class="copy-button" onClick="copyLink('${data.result.full_short_link}')">Copy</button>`
+    
+            linkList.appendChild(linkListLi);
+            localStorage.setItem("links", linkList.innerHTML);   
     });
+}
+
+function copyLink(shortLink) {
+    navigator.clipboard.writeText(shortLink);
 }
 
 function resetForm() {
