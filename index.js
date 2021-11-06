887a6ccf8a1572301dbe99c34ca9ca0872a2d4aa
Latezki <kovanen.lauri@gmail.com>
2021-11-06 22:09:30 +0200

    Finished url shortening button design and changed form warning css from visibility to display. Also removed unnecessary js.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const linkForm = document.getElementById("linkForm");
 const linkList = document.getElementById("linkList");
 const urlInput = document.getElementById("inputText");
+const inputDiv = document.getElementById("inputDiv");
 const formWarning = document.getElementById("formWarning");
-const buttonS = document.getElementsByClassName("copy-button");
+
 
 window.addEventListener("load", function() {
     const storageLinkList = localStorage.getItem("links");
@@ -11,26 +12,22 @@ window.addEventListener("load", function() {
     }
 });
 
-for (var i = 0 ; i < linkList.length; i++) {
-    buttonS[i].addEventListener("click", function(e) {
-        console.log("clicked");
-    }); 
- }
-
-
 linkForm.addEventListener("submit", function(e) {
     e.preventDefault();
 
     const urlName = urlInput.value.toLowerCase();
     console.log(urlName);
 
+    var warningText = document.createElement("p");
+    warningText.innerHTML = `<p class="form-warning" id="formWarning">Add a valid url</p>`;
+
     if (urlName.includes(".") && urlName.length > 5 && urlName.match("^[a-z0-9.]+$")) {
         newLink(urlName);
-        resetForm();
-        formWarning.style.visibility = "hidden";
+        formWarning.style.display = "none";
         urlInput.classList.remove("warning-border");
+        resetForm();
     } else {
-        formWarning.style.visibility = "visible";
+        formWarning.style.display = "block";
         urlInput.classList.add("warning-border");
     }
 });
@@ -39,12 +36,11 @@ function newLink(urlName) {
     fetch(`https://api.shrtco.de/v2/shorten?url=${urlName}`)
     .then(res => res.json())
     .then(data => {
-        linkListLi = document.createElement("li");
+        const linkListLi = document.createElement("li");
             linkListLi.className = "link-li";
             linkListLi.innerHTML = `<p class="website-link">${data.result.original_link}</p>
             <p class="shortened-link">${data.result.full_short_link}</p>
-            <button class="copy-button" onClick="copyLink('${data.result.full_short_link}')">Copy</button>`
-    
+            <button class="copy-button" onClick="copyLink('${data.result.full_short_link}'); changeButton(event);">Copy</button>`
             linkList.appendChild(linkListLi);
             localStorage.setItem("links", linkList.innerHTML);   
     });
@@ -52,6 +48,13 @@ function newLink(urlName) {
 
 function copyLink(shortLink) {
     navigator.clipboard.writeText(shortLink);
+}
+
+function changeButton(event) {
+    const btn = event.target;
+    btn.classList.remove("copy-button");
+    btn.classList.add("copied-button");
+    btn.innerHTML = "Copied!";
 }
 
 function resetForm() {
